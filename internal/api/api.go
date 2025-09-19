3f92909632cdbf173781e58e42e38cb70df5933a
yokitheyo <razvygs@gmail.com>
2025-09-20 01:46:56 +0300

    refactor: move server logic  from app to api package

---
+++ internal/api/api.go
@@ -0,0 +1,58 @@
+package api
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/wb-go/wbf/ginext"
+	"github.com/wb-go/wbf/zlog"
+	"github.com/yokitheyo/wb_level3_02/internal/app"
+)
+
+type API struct {
+	Engine *ginext.Engine
+	server *http.Server
+	App    *app.App
+}
+
+func NewAPI(a *app.App) *API {
+	engine := ginext.New()
+	engine.Use(ginext.Logger(), ginext.Recovery())
+	engine.LoadHTMLGlob("templates/*")
+
+	api := &API{
+		Engine: engine,
+		App:    a,
+	}
+
+	api.registerRoutes()
+	return api
+}
+
+func (api *API) registerRoutes() {
+	api.Engine.POST("/shorten", api.App.Service.HandleShorten)
+	api.Engine.GET("/s/:short", api.App.Service.HandleRedirect)
+	api.Engine.GET("/analytics/:short", api.App.Service.HandleAnalytics)
+}
+
+func (api *API) Start(addr string) error {
+	api.server = &http.Server{
+		Addr:    addr,
+		Handler: api.Engine,
+	}
+
+	zlog.Logger.Info().Msgf("Starting server on %s", addr)
+	return api.server.ListenAndServe()
+}
+
+func (api *API) Stop(ctx context.Context) error {
+	if api.server == nil {
+		return nil
+	}
+
+	zlog.Logger.Info().Msg("Shutting down server...")
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	return api.server.Shutdown(ctx)
+}
